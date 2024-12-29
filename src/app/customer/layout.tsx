35e57be33e3f93cada2ff33c326feb48705a39e3
zhenja <ye.ulchak@insidedynamic.de>
2024-12-29 17:46:45 +0200

    fix

--- src/app/customer/layout.tsx
+++ src/app/customer/layout.tsx
@@ -1,7 +1,8 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "../../components/footer";
 import SideBar from "../../components/sidebar";
+import { useRouter } from "next/navigation";
 
 export default function DashboardLayout({
   children,
@@ -9,9 +10,26 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   const [isSideBarOpen, setIsSideBarOpen] = useState(false);
-
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const footerPosition = isSideBarOpen ? "230px" : "0";
   const footerIndex = isSideBarOpen ? "0" : "9999";
+  const router = useRouter();
+
+  useEffect(() => {
+    // Перевірка сесії або токену
+    const token = sessionStorage.getItem('AuthToken');
+    if (!token) {
+      // Якщо токен відсутній, перенаправляємо на сторінку входу
+      router.push('/login');
+    } else {
+      setIsAuthenticated(true);
+    }
+  }, [router]);
+
+  // Якщо не авторизований, не рендеримо дочірні компоненти
+  if (!isAuthenticated) {
+    return null; // Можна також показати спінер чи повідомлення про завантаження
+  }
 
   return (
     <>
