ad0cd336e98881c5aa8df122b91af49cb5b40a7e
zhenja <super-zhenja@ukr.net>
2025-01-06 16:40:52 +0200

    fix

--- src/app/dashboard/admin/einstellungen/page.tsx
+++ src/app/dashboard/admin/einstellungen/page.tsx
@@ -68,139 +68,76 @@ const ConfigPage: React.FC = () => {
 
   return (
 // TODO FIX УДАЛИТИ МОДАЛКИ ЗРОБИТИ СИЛКИ НА ТАБЛИЦІ
+<Container className="ContainerConfigPage" maxWidth="sm">
+  <Box
+    className="BoxConfigPage"
+    display="flex"
+    flexDirection="column" // Встановлюємо вертикальне вирівнювання
+    alignItems="center" // Центруємо елементи по горизонталі
+    gap={2} // Додаємо відстань між кнопками
+  >
+    <Typography variant="h4" gutterBottom>
+      Admin
+    </Typography>
 
-    <Container className="ContainerConfigPage" maxWidth="sm">
-      <Box className="BoxConfigPage">
-        <Typography variant="h4" gutterBottom>
-            Admin
-        </Typography>
-
-        {/* Email Button */}
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => router.push("/dashboard/admin/SMTP-Email")}
-          className="ConfigPageButton"
-        >
-          <Box className="BoxConfigPageButtonIcon">
-            <EmailIcon className="ConfigPageButtonIcon" />
-            <p className="ConfigPageFont">E-Mail anpassen</p>
-          </Box>
-        </Button>
-
-        {/* DMS Button */}
-        <Button
-          variant="contained"
-          color="secondary"
-          onClick={() => router.push("/dashboard/admin/dms-config")}
-          className="ConfigPageButton"
-        >
-          <Box className="BoxConfigPageButtonIcon">
-            <FilePresentIcon className="ConfigPageButtonIcon" />
-            <p className="ConfigPageFont">DMS anpassen</p>
-          </Box>
-        </Button>
-
-        {/* Lexoffice Button */}
-        <Button
-          variant="contained"
-          color="success"
-          onClick={() => router.push("/dashboard/admin/accounting-software")}
-          className="ConfigPageButton"
-        >
-          <Box className="BoxConfigPageButtonIcon">
-            <EuroIcon className="ConfigPageButtonIcon" />
-            <p className="ConfigPageFont">Accounting-Software</p>
-          </Box>
-        </Button>
-
-        {/* Profil Button */}
-        <Button
-          variant="contained"
-          color="warning"
-          onClick={() => handleClickOnProfile()}
-          className="ConfigPageButton"
-        >
-          <Box className="BoxConfigPageButtonIcon">
-            <AccountCircleIcon className="ConfigPageButtonIcon" />
-            <p className="ConfigPageFont">Profile anpassen</p>
-          </Box>
-        </Button>
+    {/* Email Button */}
+    <Button
+      variant="contained"
+      color="primary"
+      onClick={() => router.push("/dashboard/admin/SMTP-Email")}
+      className="ConfigPageButton"
+      fullWidth // Робимо кнопку шириною на весь контейнер
+    >
+      <Box className="BoxConfigPageButtonIcon" display="flex" alignItems="center" justifyContent="center">
+        <EmailIcon className="ConfigPageButtonIcon" />
+        <p className="ConfigPageFont">E-Mail anpassen</p>
       </Box>
+    </Button>
 
-      {/* Modal */}
-      <Modal open={openModal !== null} onClose={handleClose}>
-        <Box className="modalConfigPage">
-          <Typography variant="h6" gutterBottom color="black">
-            {openModal === "email" && "E-Mail anpassen"}
-            {openModal === "dms" && "DMS anpassen"}
-            {openModal === "lexoffice" && "Lexoffice anpassen"}
-          </Typography>
+    {/* DMS Button */}
+    <Button
+      variant="contained"
+      color="secondary"
+      onClick={() => router.push("/dashboard/admin/dms-config")}
+      className="ConfigPageButton"
+      fullWidth // Робимо кнопку шириною на весь контейнер
+    >
+      <Box className="BoxConfigPageButtonIcon" display="flex" alignItems="center" justifyContent="center">
+        <FilePresentIcon className="ConfigPageButtonIcon" />
+        <p className="ConfigPageFont">DMS anpassen</p>
+      </Box>
+    </Button>
 
-          {openModal === "dms" ? (
-            <>
-              <TextField
-                fullWidth
-                margin="normal"
-                variant="outlined"
-                label="DMS suchen"
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-                InputLabelProps={{
-                  style: { color: "black", fontWeight: "bold" },
-                }}
-              />
-              <FormControl fullWidth margin="normal" >
-                <InputLabel style={{ color: "black" }} >
-                  DMS auswählen
-                </InputLabel>
-                <Select
-                  labelId="dms-select-label"
-                  value={dmsSetting}
-                  onChange={(e) => setDmsSetting(e.target.value)}
-                  variant="outlined"
-                >
-                  {filteredDmsOptions.length === 0 ? (
-                    <MenuItem disabled>Keine Ergebnisse</MenuItem> // Wenn keine Ergebnisse, zeige "Keine Ergebnisse"
-                  ) : (
-                    filteredDmsOptions.map((option) => (
-                      <MenuItem key={option} value={option}>
-                        {option}
-                      </MenuItem>
-                    ))
-                  )}
-                </Select>
-              </FormControl>
-            </>
-          ) : (
-            <TextField
-              fullWidth
-              margin="normal"
-              variant="outlined"
-              label={
-                openModal === "email" ? "Neue E-Mail" : "Lexoffice-Einstellung"
-              }
-              InputLabelProps={{
-                style: { color: "black", fontWeight: "bold" },
-              }}
-              value={openModal === "email" ? email : lexofficeSetting}
-              onChange={(e) =>
-                openModal === "email"
-                  ? setEmail(e.target.value)
-                  : setLexofficeSetting(e.target.value)
-              }
-            />
-          )}
+    {/* Lexoffice Button */}
+    <Button
+      variant="contained"
+      color="success"
+      onClick={() => router.push("/dashboard/admin/accounting-software")}
+      className="ConfigPageButton"
+      fullWidth // Робимо кнопку шириною на весь контейнер
+    >
+      <Box className="BoxConfigPageButtonIcon" display="flex" alignItems="center" justifyContent="center">
+        <EuroIcon className="ConfigPageButtonIcon" />
+        <p className="ConfigPageFont">Accounting-Software</p>
+      </Box>
+    </Button>
 
-          <Box display="flex" justifyContent="space-between" marginTop="16px">
-            <Button variant="outlined" onClick={handleClose}>
-              Abbrechen
-            </Button>
-        
-          </Box>
-        </Box>
-      </Modal>
-    </Container>
+    {/* Profil Button */}
+    <Button
+      variant="contained"
+      color="warning"
+      onClick={() => handleClickOnProfile()}
+      className="ConfigPageButton"
+      fullWidth // Робимо кнопку шириною на весь контейнер
+    >
+      <Box className="BoxConfigPageButtonIcon" display="flex" alignItems="center" justifyContent="center">
+        <AccountCircleIcon className="ConfigPageButtonIcon" />
+        <p className="ConfigPageFont">Profile anpassen</p>
+      </Box>
+    </Button>
+  </Box>
+</Container>
+
   );
 };
 
