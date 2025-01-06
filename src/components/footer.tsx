c03700222bae4b5d7ec0ea9e5a2ef70673d4f91b
zhenja <super-zhenja@ukr.net>
2025-01-06 15:41:20 +0200

    fix

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,5 @@
+
+"use client"
 import React, { useEffect, useState, useCallback } from "react";
 import { Box, Typography, Modal, Button, Container } from "@mui/material";
 import Tooltip from '@mui/material/Tooltip';
@@ -5,7 +7,10 @@ import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { enqueueSnackbar } from "notistack";
+import apiService from "@/app/services/apiService";
 
+import { useRouter } from 'next/navigation';
 
 
 
@@ -15,6 +20,24 @@ interface FooterProps {
   borderTop?: string | number;
 }
 
+type UserType = {
+  id: number;
+  tenant_id: number;
+  first_name: string;
+  last_name: string;
+  language: string;
+  username: string;
+  contact_phone: string;
+  email: string;
+  role: string;
+  is_active: boolean;
+  created_by: number | null;
+  updated_by: number | null;
+  created_at: string; // ISO 8601 формат для дати
+  updated_at: string; // ISO 8601 формат для дати
+  deleted_at: string | null; // може бути null або рядок для дати
+}
+
 // .env datei muss im Root verzeichnis gespeichert sein
 // Die .env Variablen Müssen mit NEXT_PUBLIC anfangen
 // const isDebugON = process.env.NEXT_PUBLIC_APP_DEBUG === "ON" ? true : false;
@@ -22,7 +45,8 @@ const Timeout = parseInt(process.env.NEXT_PUBLIC_APP_TIMEOUT || "300", 10);
 const LogoutViewTimer = parseInt(process.env.NEXT_PUBLIC_APP_LOGOUT_VIEW_TIMER || "20", 10);
 
 const Footer: React.FC = () => {
-
+  const router = useRouter();
+  const [User, setUser] = useState<UserType>();
   const [footerVisible, setFooterVisible] = useState(false);
   const [tooltipVisible, setTooltipVisible] = useState({
     user: false,
@@ -36,6 +60,13 @@ const Footer: React.FC = () => {
     licInfo: 'up',
     objectPage: 'up',
   });
+
+  const login_timeout = !User ? '200' : '300'
+  const page_refresh_time = !User ? '300' : '300'
+  const display_name = !User ? 'user' : '100'
+  const id = !User ? '1' : '100'
+  const [count, setCount] = useState<number>(Number(login_timeout));
+  const [refresh, setRefresh] = useState<number>(Number(page_refresh_time));
 
   const handleTooltipToggle = (key: keyof typeof tooltipVisible) => {
     setTooltipVisible((prevState) => ({
@@ -52,8 +83,8 @@ const Footer: React.FC = () => {
   };
 
 
-  const [counter, setCounter] = useState(Timeout);
-  const [showModal, setShowModal] = useState(false);
+  // const [counter, setCounter] = useState(Timeout);
+  // const [showModal, setShowModal] = useState(false);
   const getSharedObject = () => {
     const storedSetting = sessionStorage.getItem("setting");
     if (storedSetting) {
@@ -71,39 +102,129 @@ const Footer: React.FC = () => {
 
 
 
-  const resetTimer = useCallback(() => {
-    setCounter(Timeout);
-    setShowModal(false);
-    setCounter(Timeout);
-    setShowModal(false);
-  }, []);
+  // const resetTimer = useCallback(() => {
+  //   setCounter(Timeout);
+  //   setShowModal(false);
+  //   setCounter(Timeout);
+  //   setShowModal(false);
+  // }, []);
 
-  const handleLogoutLocal = () => {
-    sessionStorage.clear();
-    localStorage.clear();
-    window.location.href = "/login";
-  };
+  // const handleLogoutLocal = () => {
+  //   sessionStorage.clear();
+  //   localStorage.clear();
+  //   window.location.href = "/login";
+  // };
+
+  // useEffect(() => {
+  //   const timer = setInterval(() => {
+  //     setCounter((prev) => {
+  //       if (prev <= 1) {
+  //         clearInterval(timer);
+  //         handleLogoutLocal();
+  //         return 0;
+  //       }
+  //       if (prev <= LogoutViewTimer) setShowModal(true);
+  //       return prev - 1;
+  //     });
+  //   }, 1000);
+
+  //   window.addEventListener("mousemove", resetTimer);
+
+  //   return () => {
+  //     clearInterval(timer);
+  //     window.removeEventListener("mousemove", resetTimer);
+  //   };
+  // }, [resetTimer]);
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setCounter((prev) => {
-        if (prev <= 1) {
+      setCount((prevCount: number) => {
+        if (prevCount <= 1) {
           clearInterval(timer);
-          handleLogoutLocal();
           return 0;
         }
-        if (prev <= LogoutViewTimer) setShowModal(true);
-        return prev - 1;
+        return prevCount - 1;
       });
     }, 1000);
+    return () => clearInterval(timer);
+  }, [login_timeout]);
 
-    window.addEventListener("mousemove", resetTimer);
+  useEffect(() => {
+    if (refresh > 0) {
+      const timerRefresh = setInterval(() => {
+        setRefresh(refresh - 1);
+      }, 1000);
+      return () => clearInterval(timerRefresh);
+    }
+  }, [refresh]);
 
+
+  // const handleLogout = async () => {
+  //   const getToken: any = sessionStorage.getItem('AuthToken');
+  //   if (!getToken) {
+  //     enqueueSnackbar("Kein Token gefunden. Automatisches Weiterleiten zur Login-Seite.", {
+  //       variant: "warning"
+  //     });
+  //     router.push("/login");
+  //     sessionStorage.clear();
+  //     return;
+  //   }
+  //   try {
+  //     await apiService.get(`user/logout`, getToken);
+  //     enqueueSnackbar("Logout erfolgreich!", {
+  //       variant: "success"
+  //     });
+
+  //   } catch (error) {
+  //     enqueueSnackbar("Fehler beim Logout. Bitte versuchen Sie es später erneut.", {
+  //       variant: "error"
+  //     });
+  //   }
+
+  //   router.push("/login");
+  //   sessionStorage.clear()
+  // };
+
+
+  useEffect(() => {
+    if (count !== 0) {
+      if (refresh <= (refresh * 90) / 100) {
+        // handleLogout()
+
+        setRefresh(Number(page_refresh_time))
+      }
+    }
+  }, [refresh, page_refresh_time, count]);
+
+
+  const handleMouseMove = useCallback(() => {
+    setCount(Number(login_timeout));
+  }, [login_timeout]);
+
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouseMove);
     return () => {
-      clearInterval(timer);
-      window.removeEventListener("mousemove", resetTimer);
+      window.removeEventListener('mousemove', handleMouseMove);
     };
-  }, [resetTimer]);
+  }, [handleMouseMove]);
+
+  
+
+
+
+  useEffect(() => {
+    const userObj: any = sessionStorage.getItem('AuthUser')
+    const user: UserType = JSON.parse(userObj)
+
+    setUser(user)
+
+    if (count === 1) {
+      router.push("/login");
+      sessionStorage.clear()
+    }
+  }, [count])
+
+
 
   return (
     <Box
@@ -204,7 +325,11 @@ const Footer: React.FC = () => {
         >
           <Box sx={{ display: 'flex', justifyContent: 'center', gap: 3 }}>
             {/* Tooltip Buttons */}
-            <Tooltip title="User Info" placement="top" open={tooltipVisible.user}>
+            <Tooltip title={<div>
+              <p>email: {User?.email || 'Немає email'}</p>
+              <p>first_name: {User?.first_name || 'Немає імені'}</p>
+              <p>role: {User?.role || 'Немає ролі'}</p>
+            </div>} placement="top" open={tooltipVisible.user}>
               <Button
                 variant="text"
                 color="inherit"
@@ -225,7 +350,12 @@ const Footer: React.FC = () => {
               </Button>
             </Tooltip>
 
-            <Tooltip title="Time Info" placement="top" open={tooltipVisible.time}>
+            <Tooltip title=
+            {<div>
+              <p>Logout: {count || '0'}</p>
+              <p>refresh: {refresh || '0'}</p>
+             
+            </div>} placement="top" open={tooltipVisible.time}>
               <Button
                 variant="text"
                 color="inherit"
@@ -246,7 +376,7 @@ const Footer: React.FC = () => {
               </Button>
             </Tooltip>
 
-            <Tooltip title="License Info" placement="top" open={tooltipVisible.licInfo}>
+            {/* <Tooltip title="License Info" placement="top" open={tooltipVisible.licInfo}>
               <Button
                 variant="text"
                 color="inherit"
@@ -265,9 +395,21 @@ const Footer: React.FC = () => {
                 LIC INFO
                 {arrowDirection.licInfo === 'up' ? <ArrowUpwardIcon sx={{ color: 'white', marginLeft: 1 }} /> : <ArrowDownwardIcon sx={{ color: 'white', marginLeft: 1 }} />}
               </Button>
-            </Tooltip>
+            </Tooltip> */}
 
-            <Tooltip title="Object Page Info" placement="top" open={tooltipVisible.objectPage}>
+            <Tooltip title={
+              <div>
+                <p>email: {User?.email || 'Немає email'}</p>
+                <p>first_name: {User?.first_name || 'Немає імені'}</p>
+                <p>created_at: {User?.created_at || 'Немає ролі'}</p>
+                <p>is_active: {User?.is_active || 'Немає ролі'}</p>
+                <p>language: {User?.language || 'Немає ролі'}</p>
+                <p>tenant_id: {User?.tenant_id || 'Немає ролі'}</p>
+                <p>id: {User?.id || 'Немає ролі'}</p>
+                <p>username: {User?.username || 'Немає ролі'}</p>
+
+              </div>
+            } placement="top" open={tooltipVisible.objectPage}>
               <Button
                 variant="text"
                 color="inherit"
